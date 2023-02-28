83dd287151301a540ac4c1cc8d58efd78693d542
Severyn Kurach <severynson.kurach@gmail.com>
2023-02-28 16:49:14 +0100

    Section-10-COMLETED

--- Section 10 (SQL introduction)/util/database.js
+++ Section 10 (SQL introduction)/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: '1111'
 });
 
 module.exports = pool.promise();
